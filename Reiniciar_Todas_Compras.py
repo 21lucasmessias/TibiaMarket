23e003a69e2e2d58f6188c3b7b081e0b81fd9a43
Lucas Messias do Prado <41164252+21lucasmessias@users.noreply.github.com>
2019-08-14 13:38:43 -0300

    Delete Reiniciar_Todas_Compras.py

--- Reiniciar_Todas_Compras.py
+++
@@ -1,7 +0,0 @@
-from Todas_Compras import Todas_Compras
-
-for item in Todas_Compras:
-    item['quantity'] = 0
-
-with open('Todas_Compras.py', 'w') as file:
-    file.write(f'Todas_Compras = {Todas_Compras}\n'.replace('}, ', '},\n'))
